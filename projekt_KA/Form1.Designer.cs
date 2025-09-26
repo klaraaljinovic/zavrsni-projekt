e1409947b4ebef9f4b6566d32018a9b307ebd485
klaraaljinovic <klara.aljinovic@skole.hr>
2025-09-26 17:07:41 +0200

    Add project files.

---
+++ projekt_KA/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace projekt_KA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.unos_životinje = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pregled = new System.Windows.Forms.Button();
+            this.filteri = new System.Windows.Forms.Button();
+            this.izlaz = new System.Windows.Forms.Button();
+            this.udomljavanje = new System.Windows.Forms.Button();
+            this.statistika = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // unos_životinje
+            // 
+            this.unos_životinje.Location = new System.Drawing.Point(79, 99);
+            this.unos_životinje.Name = "unos_životinje";
+            this.unos_životinje.Size = new System.Drawing.Size(140, 69);
+            this.unos_životinje.TabIndex = 0;
+            this.unos_životinje.Text = "UNOS ŽIVOTINJE";
+            this.unos_životinje.UseVisualStyleBackColor = true;
+            this.unos_životinje.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pregled
+            // 
+            this.pregled.Location = new System.Drawing.Point(252, 99);
+            this.pregled.Name = "pregled";
+            this.pregled.Size = new System.Drawing.Size(140, 69);
+            this.pregled.TabIndex = 1;
+            this.pregled.Text = "PREGLED";
+            this.pregled.UseVisualStyleBackColor = true;
+            // 
+            // filteri
+            // 
+            this.filteri.Location = new System.Drawing.Point(672, 195);
+            this.filteri.Name = "filteri";
+            this.filteri.Size = new System.Drawing.Size(75, 40);
+            this.filteri.TabIndex = 2;
+            this.filteri.Text = "FILTERI";
+            this.filteri.UseVisualStyleBackColor = true;
+            // 
+            // izlaz
+            // 
+            this.izlaz.Location = new System.Drawing.Point(672, 49);
+            this.izlaz.Name = "izlaz";
+            this.izlaz.Size = new System.Drawing.Size(75, 23);
+            this.izlaz.TabIndex = 3;
+            this.izlaz.Text = "IZLAZ";
+            this.izlaz.UseVisualStyleBackColor = true;
+            // 
+            // udomljavanje
+            // 
+            this.udomljavanje.Location = new System.Drawing.Point(607, 99);
+            this.udomljavanje.Name = "udomljavanje";
+            this.udomljavanje.Size = new System.Drawing.Size(140, 69);
+            this.udomljavanje.TabIndex = 4;
+            this.udomljavanje.Text = "UDOMLJAVANJE";
+            this.udomljavanje.UseVisualStyleBackColor = true;
+            // 
+            // statistika
+            // 
+            this.statistika.Location = new System.Drawing.Point(419, 99);
+            this.statistika.Name = "statistika";
+            this.statistika.Size = new System.Drawing.Size(140, 69);
+            this.statistika.TabIndex = 5;
+            this.statistika.Text = "STATISTIKA";
+            this.statistika.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 495);
+            this.Controls.Add(this.statistika);
+            this.Controls.Add(this.udomljavanje);
+            this.Controls.Add(this.izlaz);
+            this.Controls.Add(this.filteri);
+            this.Controls.Add(this.pregled);
+            this.Controls.Add(this.unos_životinje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button unos_životinje;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button pregled;
+        private System.Windows.Forms.Button filteri;
+        private System.Windows.Forms.Button izlaz;
+        private System.Windows.Forms.Button udomljavanje;
+        private System.Windows.Forms.Button statistika;
+    }
+}
+
---
+++ projekt_KA/Form2.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace projekt_KA
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ime = new System.Windows.Forms.Label();
+            this.pasmina = new System.Windows.Forms.Label();
+            this.vrsta = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.musko = new System.Windows.Forms.RadioButton();
+            this.zensko = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dob = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // ime
+            // 
+            this.ime.AutoSize = true;
+            this.ime.Location = new System.Drawing.Point(40, 62);
+            this.ime.Name = "ime";
+            this.ime.Size = new System.Drawing.Size(29, 13);
+            this.ime.TabIndex = 1;
+            this.ime.Text = "IME:";
+            // 
+            // pasmina
+            // 
+            this.pasmina.AutoSize = true;
+            this.pasmina.Location = new System.Drawing.Point(40, 159);
+            this.pasmina.Name = "pasmina";
+            this.pasmina.Size = new System.Drawing.Size(58, 13);
+            this.pasmina.TabIndex = 2;
+            this.pasmina.Text = "PASMINA:";
+            this.pasmina.Click += new System.EventHandler(this.pasmina_Click);
+            // 
+            // vrsta
+            // 
+            this.vrsta.AutoSize = true;
+            this.vrsta.Location = new System.Drawing.Point(40, 111);
+            this.vrsta.Name = "vrsta";
+            this.vrsta.Size = new System.Drawing.Size(46, 13);
+            this.vrsta.TabIndex = 3;
+            this.vrsta.Text = "VRSTA:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 152);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // musko
+            // 
+            this.musko.AutoSize = true;
+            this.musko.Location = new System.Drawing.Point(43, 223);
+            this.musko.Name = "musko";
+            this.musko.Size = new System.Drawing.Size(64, 17);
+            this.musko.TabIndex = 6;
+            this.musko.TabStop = true;
+            this.musko.Text = "MUŠKO";
+            this.musko.UseVisualStyleBackColor = true;
+            // 
+            // zensko
+            // 
+            this.zensko.AutoSize = true;
+            this.zensko.Location = new System.Drawing.Point(144, 223);
+            this.zensko.Name = "zensko";
+            this.zensko.Size = new System.Drawing.Size(69, 17);
+            this.zensko.TabIndex = 7;
+            this.zensko.TabStop = true;
+            this.zensko.Text = "ŽENSKO";
+            this.zensko.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(104, 275);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // dob
+            // 
+            this.dob.AutoSize = true;
+            this.dob.Location = new System.Drawing.Point(51, 277);
+            this.dob.Name = "dob";
+            this.dob.Size = new System.Drawing.Size(30, 13);
+            this.dob.TabIndex = 9;
+            this.dob.Text = "DOB";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(43, 353);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "DATUM DOLASKA:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(43, 409);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "CIJEPLJEN";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(144, 409);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(88, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "KASTRIRAN";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 451);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "NAPOMENA:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dob);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.zensko);
+            this.Controls.Add(this.musko);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.vrsta);
+            this.Controls.Add(this.pasmina);
+            this.Controls.Add(this.ime);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ime;
+        private System.Windows.Forms.Label pasmina;
+        private System.Windows.Forms.Label vrsta;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton musko;
+        private System.Windows.Forms.RadioButton zensko;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label dob;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
