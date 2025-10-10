f704c3248ebd17a5a603ba7e18bd19be9ed1b665
klaraaljinovic <klara.aljinovic@skole.hr>
2025-10-10 17:24:51 +0200

    dovršeni dizajnovi forme

--- projekt_KA/Form6.cs
+++ projekt_KA/Form6.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace projekt_KA
 {
-    public partial class Form1 : Form
+    public partial class Form6 : Form
     {
-        public Form1()
+        public Form6()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
